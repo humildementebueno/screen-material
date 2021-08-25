4f0d7dc4408305ae0a873d03f6f1b7016e6ef927
Eber <doneber98@gmail.com>
2021-08-24 22:50:29 -0400

    Add option to close and open a dialog with 'devolcionales'

---
+++ script.js
@@ -0,0 +1,8 @@
+function openDevocionalesModal() {
+    document.querySelector('#dialog-video').style.display = "block"
+}
+
+function closeDevocionalesModal() {
+    document.querySelector('#dialog-video').style.display = "none"
+    
+}

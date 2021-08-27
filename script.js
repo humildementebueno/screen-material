eb8485dd08c68db6d52a00387e288ab6332f0e18
Eber <doneber98@gmail.com>
2021-08-26 22:53:08 -0400

    fix error with dialog

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function openDevocionalesModal() {
     document.querySelector('#dialog-video').style.display = "block"
+    document.querySelector('.dialog-background').style.display = "block"
 }
 
 function closeDevocionalesModal() {
     document.querySelector('#dialog-video').style.display = "none"
+    document.querySelector('.dialog-background').style.display = "none"
     
 }
